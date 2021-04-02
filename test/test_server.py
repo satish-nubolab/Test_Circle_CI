7c424490f550079b685aaaa6e35f5881ff4572df
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-04-02 16:18:41 +0530

    Update test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -1,5 +1,13 @@
 from hello import app
+import unittest
+from src.sample import Person
+
 with app.test_client() as c:
     print("****************************Hello Circle CI***************************")
     response = c.get('/')
     assert response.data == b'Hello World!'
+
+class Test(unittest.TestCase):
+    def test_first():
+        p = Person('abc')
+        assertEqual(30, p.process_input(10, 20, 'add'))
