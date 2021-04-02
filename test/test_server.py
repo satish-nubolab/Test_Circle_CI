4e1127f5c94b739596665f9041e7582db30da64b
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-04-02 16:34:40 +0530

    Update test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -11,4 +11,22 @@ with app.test_client() as c:
 class Test(unittest.TestCase):
     def test_first(self):
         p = Person('abc')
+        self.assertEqual(100, p.process_input(10, 10, 'multiple'))
+    def test_sec(self):
+        p = Person('abc')
         self.assertEqual(30, p.process_input(10, 20, 'add'))
+    def test_third(self):
+        p = Person('abc')
+        self.assertEqual(5, p.process_input(100, 20, 'divide'))
+    def test_fourth(self):
+        p = Person('abc')
+        self.assertEqual(30, p.process_input(50, 20, 'subtract'))
+    def test_fifth(self):
+        p = Person('abc')
+        self.assertEqual(120, p.process_input(100, 20, 'add'))
+    def test_sixth(self):
+        p = Person('abc')
+        self.assertEqual(200, p.process_input(100, 100, 'add'))
+    def test_seventh(self):
+        p = Person('abc')
+        self.assertEqual(30, p.process_input(10, 200, 'add'))        
