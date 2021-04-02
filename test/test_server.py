0e726a316428fb3327cba3abca2496b9aedbca04
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-04-02 15:44:48 +0530

    Update test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -3,4 +3,3 @@ with app.test_client() as c:
     print("****************************Hello Circle CI***************************")
     response = c.get('/')
     assert response.data == b'Hello World!'
-    
