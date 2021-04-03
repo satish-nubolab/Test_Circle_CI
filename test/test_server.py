2f757c5a171271128550033aea8f3aaf2b57802f
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-04-03 09:17:58 +0530

    Update test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
         self.assertEqual(5, p.process_input(100, 20, 'divide'))
 
     def test_fourth(self):
-        p = Person('abc')    
+        p = Person('abc')
         self.assertEqual(30, p.process_input(50, 20, 'subtract'))
 
     def test_fifth(self):
@@ -35,4 +35,4 @@ class Test(unittest.TestCase):
 
     def test_seventh(self):
         p = Person('abc')
-        self.assertEqual(30, p.process_input(10, 200, 'add'))        
+        self.assertEqual(30, p.process_input(10, 200, 'add'))
